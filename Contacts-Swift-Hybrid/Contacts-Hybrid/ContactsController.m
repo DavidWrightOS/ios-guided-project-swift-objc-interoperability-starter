05d39923885c8528e7c27bf9186e63a427f172be
David Wright <davidtwright89@gmail.com>
2020-06-02 21:45:34 -0400

    Added a model controller and bridging header

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsController.m
@@ -0,0 +1,13 @@
+//
+//  ContactsController.m
+//  Contacts-Hybrid
+//
+//  Created by David Wright on 6/2/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "ContactsController.h"
+
+@implementation ContactsController
+
+@end
