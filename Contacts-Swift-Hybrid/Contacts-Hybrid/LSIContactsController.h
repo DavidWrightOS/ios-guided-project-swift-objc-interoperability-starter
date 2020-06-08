f784d9d247516512f2003fdf9e7399156aab4419
David Wright <davidtwright89@gmail.com>
2020-06-07 21:03:26 -0400

    Added helpful comments to explain what is going on in LSIContactsController

--- Contacts-Swift-Hybrid/Contacts-Hybrid/LSIContactsController.h
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/LSIContactsController.h
@@ -10,9 +10,20 @@
 
 @class LSIContact;
 
+// NS_SWIFT_NAME(SwiftName) - Rename an Objective-C class for use in Swift
+// Macros are always named using all caps
 NS_SWIFT_NAME(ContactsController)
 @interface LSIContactsController : NSObject
 
+// Nullability Flags - change how the object is imported into Swift
+
+// nullable           ->  [Contact]?
+// nonnull            ->  [Contact]
+// null_resettable    ->  [Contact]?
+// null_unspecified*  ->  [Contact]!    // this is the default
+
+// Use lightweight generics to provide type information (so it's easier to use in Swift)
+//@property (nonatomic ,readonly, copy, nonnull) NSArray *contacts;
 @property (nonatomic ,readonly, copy, nonnull) NSArray<LSIContact *> *contacts;
 
 - (nonnull LSIContact *)contactAtIndex:(NSInteger)index;
