51dd5f782fbd016da4282a932d48fd31fd7d11e5
David Wright <davidtwright89@gmail.com>
2020-06-07 21:02:18 -0400

    Added helpful comments to explain what is going on in the Contact class

--- Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
@@ -8,12 +8,27 @@
 
 import Foundation
 
-// @objcMembers
+// To use this in Objective-C we need to subclass from NSObject
+// Swift Structs are not compatible with Objective-C structures
+
+// @objc                   - Exposes a class and any of its properties or methods
+// @objcMembers            - Apply @objc to entire class including all of its properties and methods
+// @objc(newNameForObjc)   - Rename class, properties, and methods for use in Objective-C
 @objc(LSIContact)
 class Contact: NSObject {
     
-    @objc var name: String
-    @objc var relationship: String?
+    @objc var name: String // nonnull
+    @objc var relationship: String? // nullable
+    
+    // If you want to expose the default initializer to Objective-C
+    // you must Must override and implement it with the @objc pre-fix
+    /*
+    @objc override init() {
+        self.name = ""
+        self.relationship = nil
+        // no need to call super.init() here because Swift will add it for you behind the scenes
+    }
+    */
     
     @objc init(name: String, relationship: String?) {
         self.name = name
